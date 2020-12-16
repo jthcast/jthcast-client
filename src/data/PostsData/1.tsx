19223632cea8629c70c27adf7ef135214810bcbe
jthcast <jthcast@gmail.com>
2020-12-16 20:05:02 +0900

    Fix remove useless variables

--- src/data/PostsData/1.tsx
+++ src/data/PostsData/1.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PrismCode from '../../components/atoms/PrismCode';
 import Switch from '../../components/atoms/Switch';
 import DarkModeSwitch from '../../components/molecules/DarkModeSwitch';
 import structure from '../../resources/images/post/1/structure.png';
@@ -7,70 +6,6 @@ import header from '../../resources/images/post/1/header.jpg';
 import figmaSwitch from '../../resources/images/post/1/figma switch.png';
 
 const Item = (): JSX.Element => {
-  const switchCode = `import React from 'react';
-import { IconSpinner } from '../Icons';
-import './Switch.scss';
-
-interface SwitchProps {
-  ariaLabel?: string;
-  checked?: boolean;
-  checkedChildren?: string;
-  className?: string;
-  disabled?: boolean;
-  loading?: boolean;
-  onChange?: (event: React.FormEvent<HTMLButtonElement>) => void;
-  onClick?: (event: React.FormEvent<HTMLButtonElement>) => void;
-  tabIndex?: number;
-  unCheckedChildren?: string;
-}
-
-const Switch = ({
-  ariaLabel,
-  checked = false,
-  checkedChildren,
-  className,
-  disabled = false,
-  loading = false,
-  onChange,
-  onClick,
-  tabIndex,
-  unCheckedChildren,
-}: SwitchProps): JSX.Element => {
-  const onClickHandling = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    if (onClick) {
-      onClick(event);
-    }
-    if (onChange) {
-      onChange(event);
-    }
-  };
-
-  return (
-    <button
-      aria-checked={checked}
-      aria-label={ariaLabel}
-      className={\`jth-switch\${checked ? ' jth-switch-checked' : \`\`}\${loading || disabled ? ' jth-switch-disabled' : \`\`}\${className ? \` \${className}\` : \`\`}\`}
-      disabled={loading || disabled || false}
-      onChange={onClickHandling}
-      onClick={onClickHandling}
-      role="switch"
-      tabIndex={tabIndex}
-      type="button"
-    >
-      <div className="jth-switch-handle">{loading && <IconSpinner spin />}</div>
-      <span className="jth-switch-inner">
-        {unCheckedChildren && !checked && unCheckedChildren}
-        {checkedChildren && checked && checkedChildren}
-      </span>
-    </button>
-  );
-};
-
-export default Switch;
-
-`;
   return (
     <>
       <h2>왜 Atomic Design?</h2>
